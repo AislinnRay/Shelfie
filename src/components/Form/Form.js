00460d88ed9b56dd8c8e18ce8b678576dc0c3e08
AislinnRay <aislinn.ray.m@gmail.com>
2020-05-29 14:05:10 -0600

    added cancel and add buttons/inputs on Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,44 @@
 import React, {Component} from 'react'
+import axios from 'axios'
  
 class Form extends Component{
     constructor(){
         super()
         this.state = {
- 
+            name: '',
+            price: '',
+            image: ''
         }
     }
+
+    // componentDidMount() {}
+
+    // componentDidUpdate() {}
+
+    handleChange = ({name, value}) => {
+        this.setState({[name]: value})
+    }
+
+    handleAdd = () => {
+        const {name, price} = this.state
+        axios.post("api/products", {name, price})
+        .then(() => this.props.history.push("/"))
+    }
+
+    resetState = () => this.setState({name: "", price: "", image: ""});
+
+
     render(){
-        
+        const { name, price } = this.state
         return(
-            <div>Form</div>
+            <div>
+                <input placeholder="product name" name="name" value={name} 
+                    onChange={(e) => this.handleChange(e.target)}/>
+                <input placeholder="product price" name="price" value={price} 
+                    onChange={(e) => this.handleChange(e.target)}/>
+                <button onClick={this.resetState}>Cancel</button>
+                <button onClick={this.handleAdd}>Add to Inventory</button>
+            </div>
         )
     }
 }
